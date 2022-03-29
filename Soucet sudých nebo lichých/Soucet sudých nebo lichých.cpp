12cad1a507d0fbe872d8c4c13da956397642746d
matěj <matj@DESKTOP-1HI5AKA>
2022-03-29 16:22:58 +0200

    Přidejte soubory projektu.

---
+++ Soucet sudých nebo lichých/Soucet sudých nebo lichých.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int soucet(cislo) {
+    
+    int suda = 1;
+    for (int i = 1; i <= cislo; i += 2) {
+    
+        suda += i;
+    }
+    return suda;
+}
+
+int main() {
+    
+    printf("Soucet sudych: %d\n", soucet(5));
+}
